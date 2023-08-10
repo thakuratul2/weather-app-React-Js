a7d7b5dd8b48c89f1c59bf8602d72d21e6aac3ab
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-10 20:46:12 +0530

    Tailwind Install

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <div className="app">
+        <h1 className='text-2xl font-bold text-red-500'>Hello World</h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Temp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Temp = () => {
+  return (
+ <>
+    <div className="boc">
+        
+    </div>
+ </>
+  );
+}
+
+export default Temp;
