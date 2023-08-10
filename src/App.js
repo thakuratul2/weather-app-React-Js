8c504a5b02c9d82be0a66fd22e43a4c62a21030a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-10 20:52:09 +0530

    More Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
 const App = () => {
   return (
     <>
-      <div className="app">
-        <h1 className='text-2xl font-bold text-red-500'>Hello World</h1>
+      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
+      shadow-gray-400">
+
       </div>
     </>
   );
