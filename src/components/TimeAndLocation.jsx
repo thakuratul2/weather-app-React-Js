0e32c2d0bf1caf7df62e58041f8df0bf5e9a4bbf
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-13 12:03:24 +0530

    Done

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const TimeAndLocation = () => {
   return (
-    <div>
-      <h1>Time</h1>
+    <>
+
+   <div>
+
+ 
+    <div className='flex item-center justify-center my-6'>
+
+    <p className='text-white text-xl font-extralight'>
+      Sunday, 13 August 2023 | Local Time: 12:00 PM
+    </p>
+
     </div>
+    <div className='flex items-center justify-center my-3'>
+    <p className="text-white text-3xl font-medium">
+      Meerut, Uttar Pradesh
+    </p>
+    </div>
+    </div>
+    </>
   );
 }
 
