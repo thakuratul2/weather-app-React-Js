c0311613f55b3d97d3196a69d489f145873131ce
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-14 20:23:31 +0530

    Details Done

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UilTemperature } from '@iconscout/react-unicons'
+import { UilTemperature,UilTemperaturePlus,UilTemperatureMinus,UilRaindrops,UilWind,UilSunset, UilBrightness  } from '@iconscout/react-unicons'
 
 const Details = () => {
   return (
@@ -21,7 +21,43 @@ const Details = () => {
             Real Feel:
             <span className='font-medium ml-1'>32°</span>
             </div>
+            <div className='flex font-light text-sm items-center justify-center'>
+            <UilRaindrops size={18} className='mr-1' />
+            
+            Humidity:
+            <span className='font-medium ml-1'>65%</span>
+            </div>
+            <div className='flex font-light text-sm items-center justify-center'>
+            <UilWind size={18} className='mr-1' />
+            
+            Wind Speed:
+            <span className='font-medium ml-1'>11 km/h</span>
+            </div>
         </div>
+      </div>
+      <div className='flex flex-row items-center justify-center space-x-2
+      text-white text-sm py-3'>
+      <UilBrightness />
+      <p className="font-light">
+        Rise: <span className='font-medium ml-1'>05:45 AM</span>
+      </p>
+
+      <p className='font-light'>|</p>
+      <UilSunset />
+      <p className="font-light">
+        Sunset: <span className='font-medium ml-1'>05:45 PM</span>
+      </p>
+      <p className='font-light'>|</p>
+      <UilTemperaturePlus />
+      <p className="font-light">
+        Maximum: <span className='font-medium ml-1'>32°</span>
+      </p>
+      <p className='font-light'>|</p>
+      <UilTemperatureMinus />
+      <p className="font-light">
+        Minimum: <span className='font-medium ml-1'>39°</span>
+      </p>
+      <p className='font-light'>|</p>
       </div>
     </div>
   );
