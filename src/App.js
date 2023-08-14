dde2ce6b841fe3b4e24bb53571ea959f029c8715
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-14 20:27:36 +0530

    Header Update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
       shadow-gray-400">
-
+      <h2 className='font-medium text-2xl ml-1 text-orange-500 text-center'>Welcome To Atul Forecast Website</h2>
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
