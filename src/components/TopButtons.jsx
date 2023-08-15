fef13f778d7ff1980a44f16053aaf305914eedbb
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-16 00:30:04 +0530

    FIX

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -18,11 +18,12 @@ const TopButtons = () => {
     }
   ]
   return (
+    
     <>
       <div className="flex items-center justify-around my-6">
       
         {cities.map((city)=>
-        
+
         (
           <button key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
         
