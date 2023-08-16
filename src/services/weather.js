a5cf5f837f64102df12a99b171618ac739925b84
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-16 19:56:28 +0530

    Services

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,5 +1,6 @@
 const API_KEY = "b77ea807bcbc8991760b377a6c26696f";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
+import {DateTime} from 'luxon';
 
 const getData = (infoType, searchParam) =>{
     const url = new URL(BASE_URL + "/" + infoType);
@@ -28,14 +29,32 @@ const formatCurrentData = (data) => {
     return {lat,lon,temp,feels_like,temp_min,temp_max,humidity,name,dt,country,sunrise,sunset
     ,details,icon,speed}
 }
+
+const formatForecast = (data) => {
+    let {timezone, daily, hourly} = data;
+
+    daily = daily.slice(1,6).map()
+}
+
 const formattedData = async (searchParam) => {
 
     const formattedCurrentData = await getData('weather',
     searchParam).then(formatCurrentData)
 
+    const {lat,lon} = formattedCurrentData
+
+    const formattedForecast = await getData('onecall',{
+        lat, lon, exclude: 'current,minutely,alerts', units:searchParam.units
+    }).then(formatForecast);
+
+
     
 return formattedCurrentData
 
-}
+};
+
+const formatLocalTime = (secs,zone,format="cccc,dd LLL yyyy'
+ | Local time: 'hh:mm a") => DateTime.fromSeconds()
+
 
 export default formattedData
